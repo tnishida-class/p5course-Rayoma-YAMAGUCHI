9177f0217886dc94cfdf56702a6c461393522e16
Rayoma-YAMAGUCHI <ryamaguchi0717@gmail.com>
2021-10-13 23:13:07 +0900

    Change_sectionq2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,23 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if(i%2 + j%2 == 1){
+      fill(128);
+      rect(size *i, size *j, 25, 25);
+      }
+
+      if(i%2 + j%2 == 1){
+        if(j<3){
+        fill(255,0,0);
+        ellipse(size*(i+0.5), size *(j+0.5) , 20);
+        }
+        else if (j>4) {
+        fill(0);
+        ellipse(size*(i+0.5), size *(j+0.5) , 20);
+        }
+      }
     }
   }
 }
