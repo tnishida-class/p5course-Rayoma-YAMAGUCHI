91fa6122f5e751f2475a67d281ed2871621a7e37
Rayoma-YAMAGUCHI <ryamaguchi0717@gmail.com>
2021-10-14 23:46:46 +0900

    Change_section-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -9,6 +9,12 @@ function setup() {
 
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+    if(i%2 == 0){
+      fill(blue);
+    }
+    else{
+      fill(255);
+    }
     rect(0, i * d, width, (i + 1) * d);
   }
 
@@ -17,5 +23,10 @@ function setup() {
   rect(0, 0, size, size);
 
   fill(255);
+  stroke(255);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  for(let i = 0; i < 5; i++){
+  rect(d*2, d*i, d, d);
+  rect(d*i, d*2, d, d);
+}
 }
