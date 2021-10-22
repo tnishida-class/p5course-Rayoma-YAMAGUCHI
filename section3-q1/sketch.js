14a3de6f34f4730760165b960611004ce8c86fd7
Rayoma-YAMAGUCHI <ryamaguchi0717@gmail.com>
2021-10-22 20:18:55 +0900

    Change_section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -5,12 +5,14 @@ let cycle;
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 100;
+  cycle = 120;
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+  count = (count + 2.5) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  ellipse(width / 2, height / 2, count);
+  stroke(255);
+  ellipse(width / 2, height / 2, 80);
 }
