1e5c7bf9aa3e01b35f984b3ea06facd139412734
Rayoma-YAMAGUCHI <ryamaguchi0717@gmail.com>
2021-11-16 21:42:58 +0900

    Change_section5-3

--- section5-3/sketch.js
+++ section5-3/sketch.js
@@ -1,14 +1,79 @@
 // テキスト「関数を作る(2) 結果を戻す関数」～「総仕上げ：カレンダーを描画しよう」
 function setup(){
-  createCanvas(200, 200);
-  calendar(2019, 10);
+  createCanvas(windowWidth, windowHeight);
+  calendar(2021, 11);
 }
 
 function calendar(y, m){
-  let dow = dayOfWeek(y, m, 1);
-  for(let d = 1; d <= daysInMonth(y, m); d++){
-    // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
+  let width = windowWidth * 0.1;
+  let height = windowHeight * 0.1;
+// カレンダーの大きさ
+  rect(width, height, width * 7, height * 7);
+// カレンダーの格子
+  for(i = 0; i < 5; i++){
+    noFill();
+    stroke(3);
+    rect(width, height * 4 + i * height, width * 7, height);
   }
+  for(i = 0; i < 7; i++){
+    noFill();
+    stroke(3);
+    rect(width + i * width, height * 4, width, height * 5);
+    text(dayOfWeekAsString(i), width * 1.4 + i * width, height * 3.75);
+  }
+
+// dowの宣言：日曜が0、月曜が1、....
+  let dow = 0;
+  if(y >= 1977){
+     dow = dayOfWeek(y, m, 1);
+    if(dow > 6){
+      dow -= 7;
+    }else{dow = dow}
+  }else{
+     dow = dayOfWeek(y, m, 1);
+  }
+
+// １日の座標 x1, y1
+  let x1 = 1.05 * width + (dow - 1) * width;
+  let y1 = height * 4.25;
+// 31日分をテキスト
+  for(let d = 1; d <= daysInMonth(y, m); d++){
+    x1 += width;
+    if(x1 > width * 8){
+      x1 -= width * 7;
+      y1 += height;
+    }
+    if(y1 >= height * 9){
+      for(let i = 0; i < 7; i++){
+        rect(width + width * i, height * 9, width, height);
+      }
+    }
+    text(d, x1, y1);
+  }
+
+  textSize(height* 0.5);
+  fill(0);
+  text(y + " 年 " + m + " 月", width * 1.25, height * 2.25);
+
+  let reiwa = y - 2018;
+  let heisei = y - 1988;
+  let showa = y - 1925;
+  if(y >= 2020){
+    text("令和 " + reiwa + " 年" , width * 1.25, height * 1.75);
+  } else if (y == 2019 && m >= 5){
+    text("令和 " + reiwa + " 年" , width * 1.25, height * 1.75);
+  } else if (y == 2019 && m <= 4) {
+    text("平成 " + heisei + " 年" , width * 1.25, height * 1.75);
+  } else if (y >= 1989 && y < 2019 ) {
+    text("平成 " + heisei + " 年" , width * 1.25, height * 1.75);
+  } else if (y >= 1927 && y < 1989 ) {
+    text("昭和 " + showa + " 年" , width * 1.25, height * 1.75);
+  }
+
+  else {
+    text("かなり前", width * 1.25, height * 1.75);
+  }
+
 }
 
 function isLeapYear(y){
@@ -71,4 +136,8 @@ function dayOfWeek(y, m, d){
 function dayOfWeekAsString(dow){
   const a = ["日", "月", "火", "水", "木", "金", "土", "日", "月", "火", "水", "木", "金"];
   return a[dow];
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
 }
